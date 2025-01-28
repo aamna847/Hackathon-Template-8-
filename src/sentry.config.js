97472f830fd2149bddfa3af6b859a7ea2a8dd576
aamna847 <ranasanam464@gmail.com>
2025-01-29 00:03:23 +0500

    resolved

---
+++ src/sentry.config.js
@@ -0,0 +1,11 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
+const moduleExports = {
+    // Your existing module.exports
+};
+
+const SentryWebpackPluginOptions = {
+    silent: true, // Suppresses all logs
+};
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
